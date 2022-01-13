81440e42eee5a9634e037a0cc0e953e6d5f9ccf5
KayStation <matt0122@comcast.net>
2022-01-12 22:59:07 -0600

    Added game logic.

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -5,10 +5,12 @@ let gameMechanics = {
 
 let computerPlay = function(){
     const randomChoice = Math.floor(Math.random()*gameMechanics.playOptions.length);
-    console.log(`Number decided is ${randomChoice}.`);
+    // console.log(`Number decided is ${randomChoice}.`);
 
     const computerChoice = gameMechanics.playOptions[randomChoice];
-    console.log(`The computer played ${computerChoice}!`)
+    // console.log(`The computer played ${computerChoice}!`)
+
+    return computerChoice;
 }
 
 let humanPlay = function(){
@@ -18,29 +20,85 @@ let humanPlay = function(){
     decision = decision.toLowerCase();
 
     const logChoice = function(){
-        console.log(`You have chosen ${playerChoice}!`)
+        console.log(`You have chosen ${decision}!`)
     };
 
-    let playerChoice;
     
-    // Change playerChoice to whatever they chose, or nothing.
+    // Change decision to whatever they chose, or nothing.
     switch (decision){
         case 'rock':
-            playerChoice = 'rock';
             logChoice();
+            return 'rock';
             break;
 
         case 'paper':
-            playerChoice = 'paper';
             logChoice();
+            return 'paper';
             break;
 
         case 'scissors':
-            playerChoice = 'scissors';
             logChoice();
+            return 'scissors';
             break;
 
         default:
             console.log(`You know, you can't play ${decision} in Rock Paper Scissors.`);
+            return decision;
+    }
+}
+
+function playGame(){
+    let winner;
+    let human;
+    let computer;
+
+    let humanChoice = humanPlay();
+    console.log(`Human played ${humanChoice}`);
+
+    let computerChoice = computerPlay();
+    console.log(`Computer played ${computerChoice}`);
+    
+    const computerWinLog = `Computer played ${computerChoice} and won.`;
+    const humanWinLog = `Human played ${humanChoice} and won.`;
+
+
+    // Decide winner
+    if(humanChoice == computerChoice){
+        console.log('Game tied.');
+
+        // Results if human chose rock
+    } else if (humanChoice == 'rock'){
+
+        if(computerChoice == 'paper'){
+            winner = computer;
+            console.log(computerWinLog);
+        } else {
+            winner = human;
+            console.log(humanWinLog);
+        }
+
+        // Results if human chose paper
+    } else if (humanChoice == 'paper'){
+
+        if(computerChoice == 'scissors'){
+            winner = computer;
+            console.log(computerWinLog);
+        } else {
+            winner = human;
+            console.log(humanWinLog);
+        }
+        // Results if human chose scissors
+    } else if (humanChoice == 'scissors'){
+
+        if(computerChoice == 'rock'){
+            winner = computer;
+            console.log(computerWinLog);
+        } else {
+            winner = human;
+            console.log(humanWinLog);
+        }
+        // Results if human chose anything else.
+    } else if (humanChoice == decision){
+        console.log(`You know, ${decision} isn't a playable option.`);
     }
 }
